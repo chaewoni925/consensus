0f4fbd81ca40876d696c0c0eeb44f84da53ebeff
chaewoni925 <chaewoni925@gmail.com>
2026-09-08 13:11:52 +0900

    Git 이메일 설정 수정

---
+++ src/main/java/com/example/demo/global/config/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.demo.global.exception;
+
+import com.example.demo.global.notification.DiscordNotifier;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class GlobalExceptionHandler {
+
+    private final DiscordNotifier discordNotifier;
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e, HttpServletRequest request) {
+        String detail = request.getMethod() + " " + request.getRequestURI() + "\n" + e.toString();
+        discordNotifier.sendError("서버 에러 발생", detail);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("서버 오류가 발생했습니다.");
+    }
+}
